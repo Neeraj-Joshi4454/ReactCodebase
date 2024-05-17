3b849ed5e22fb24a7249780154ce7dc5091001e1
Pratibha <pratibha.kp96@gmail.com>
2024-05-17 16:48:44 +0530

    Initial Commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Card({val}) {
+    const star =[];
+        for(let i = 0;i<val.rating;i++){
+            star.push("*");
+        }
+      console.log(star)
+    
     return (
         <div className="border w-72 m-5 h-[500px] rounded-lg shadow-md">
             <img src={val.cover} className="w-[100%] h-[350px] rounded-t-lg" alt={val.title} />
             <div className="p-3">
                 <h1 className="text-4xl font-sans">{val.title}</h1>
-                <p className="text-gray-600 my-3">{val.rating}</p>
+                <p className="text-gray-600 my-3">{star}</p>
                 <a href={val.link} className="w-20 h-20 border p-2 bg-black text-white rounded-lg shadow-md">Watch Now</a>
             </div>
         </div>
